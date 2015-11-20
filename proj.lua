c36f81474274932d75f46c6986d4c9bf5a415c36
Your Name <m161518@usna.edu>
2015-11-20 11:23:09 -0500

    Created functionality for adding in moves

--- proj.lua
+++ proj.lua
@@ -10,21 +10,18 @@ winning["Rock"] = "Scissor"
 winning["Paper"] = "Rock"
 winning["Scissor"] = "Paper"
 
-
+numMoves = 3
 
 function intro()
 	print("\nWelcome to Rock-Paper-Scissors!")
 	print("options:")
 	print("p\tPlay the game\n")
+	print("a\tAdd moves (e.g. Superman, God, Dr. Albing\n")
 	local input = tostring(io.read())
 	return input
 end
 
 function get_move(inp)
-	while inp ~= "P" and inp ~= "R" and inp ~= "S" and inp ~= "Q" do
-		print("Please enter either R, P, or S")
-		inp = tostring(io.read())
-	end
 	return inp
 end
 
@@ -53,8 +50,6 @@ function make_move(inp)
 	
 end
 
-
-
 function play()
 		print("You are now playing Rock-Paper-Scissors against an advanced ai, good luck!")
 		print("Enter (R)ock, (P)aper, (S)cissors - R or P or S. Q will end the game\n")
@@ -65,14 +60,35 @@ function play()
 			inp = get_move(tostring(io.read()))
 		end
 
-
-
-
 end
 
+function addMove()
+                print("Enter the string representation of your new move\n")
+                local name = tostring(io.read())                
+                print("Enter the symbol representation for your move, (e.g. K for Kryptonite)\n")
+                local symbol = tostring(io.read())
 
+                print("Enter which moves your new move beats.\nThese are the current moves:")
+                for key,value in pairs(normal_moves) do 
+                    print(value)
+                end
+                
+                normal_moves[symbol] = name                    
 
-input = intro()
-if input == "p" then
-	play()
+                print("Hit enter after inputting each move, and type done when done")
+                local entry = tostring(io.read())
+                while entry ~= "done" do
+                      winning[name] = entry
+                      entry = tostring(io.read())
+                end              
+end
+
+while input ~= "p" do
+      input = intro()    
+      if input == "p" then
+         play()
+      end
+      if input == "a" then
+         addMove()
+      end
 end
